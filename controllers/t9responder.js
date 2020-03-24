53501f0e1601b63d99c8a9596a09b43eeb9186b1
Tomáš Javorský <tomasjavorsky@users.noreply.github.com>
2020-03-24 21:33:44 +0100

    combinations

--- controllers/t9responder.js
+++ controllers/t9responder.js
@@ -1,11 +1,39 @@
-const dummyResponse = (req, res) => {
+const wordResponder = (req, res) => {
 
     const {
         pressed_numbers,
     } = req.query;
 
+    const letterBuckets = {
+        2: ["a","b","c"],
+        3: ["d","e","f"],
+        4: ["g","h","i"],
+        5: ["j","k","l"],
+        6: ["m","n","o"],
+        7: ["p","q","r","s"],
+        8: ["t","u","v"],
+        9: ["w","x","y","z"],
+    }
+
+
+    const wordGenerator = () => {
+        const pressedNumbersArray = pressed_numbers.toString().split("").map(num => parseInt(num));
+        let currentArray = [""];
+        let newArray = [];
+        pressedNumbersArray.forEach((element,index)=>{
+            letterBuckets[element].forEach(letter => {
+                currentArray.forEach(currentWord => {
+                    newArray.push(`${currentWord}${letter}`)
+                })
+            })
+            currentArray = [...newArray];
+            newArray = [];
+        })
+        return currentArray;
+    }
+
     if(pressed_numbers){
-        return res.status(200).json(`Numbers pressed ${pressed_numbers}`)
+        return res.status(200).json(wordGenerator())
     }
     else{
         return res.status(400).json(`Error - did not receive any numbers`)
@@ -13,5 +41,5 @@ const dummyResponse = (req, res) => {
 }
 
 module.exports={
-    dummyResponse: dummyResponse,
+    wordResponder: wordResponder,
   };
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 // --------------CONTROLLERS---------------------
-const t9responder = require('./controllers/t9responder');
+const t9responder = require("./controllers/t9responder");
 
 // --------------SETUP---------------------
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 // --------------ROUTES---------------------
-app.get('/', (req, res) => {t9responder.dummyResponse(req, res)});
-app.get('/test', (req, res) => {res.send('kiwi api running')})
+app.get("/", (req, res) => {
+  t9responder.wordResponder(req, res);
+});
 
 app.listen(process.env.PORT || 3001);
